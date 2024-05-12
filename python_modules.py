598700ee90b53fbe33bcf5f57825b793463df6e6
Neil Patel <neilpatel235@gmail.com>
2024-05-12 19:53:11 -0400

    importing code, MODULES

---
+++ python_modules.py
@@ -0,0 +1,16 @@
+# rule of thumb, just import the things you need
+# as you need more, just edit the imports
+
+import random
+import random as omg_so_random
+from random import *
+from random import choice, shuffle
+from random import choice as gimme_one, shuffle as mix_up_fruits
+
+#    file_1.py                         file_2.py
+
+# def fn():                        import file_1
+#     return "do some stuff"       file_1.fn()    # 'do some stuff'
+#
+# def other_fn():                  file_1.other() # 99
+#     return 99
