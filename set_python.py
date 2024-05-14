e0a5e9af035fbee70b7da7d4a990642270dea179
Neil Patel <neilpatel235@gmail.com>
2024-05-14 16:27:17 -0400

    sets

--- set_python.py
+++ set_python.py
@@ -5,6 +5,9 @@
 # every item in a set is unique
 # cannot access using index
 
+
+
+
 # <---- Accessing and Creating ---->
 print("\n<---- Accessing and Creating ---->")
 
@@ -22,12 +25,18 @@ if True in m:
 for thing in m:
     print("<>", thing, end="")
 
+
+
+
 # <---- something cool ---->
 print("\n<---- something cool ---->")
 
 places = ['North', 'South', 'West', 'East', 'West', 'North', 'South']
 simple = list(set(places))
 print(simple)
+
+
+
 
 # <---- Methods ---->
 print("\n<---- Methods ---->")
@@ -50,6 +59,9 @@ print(new1)
 new2 = math_students & biology_students
 print(new2)
 
+
+
+
 # <---- Set Comprehension ---->
 print("\n<---- Set Comprehension ---->")
 
@@ -60,3 +72,20 @@ print(check, next)
 
 def check_all_vowels_in_s(check_string):
     return len({c for c in check_string if c in 'aeiou'}) == 5
+
+
+
+# <---- summary ---->
+print("\n<---- summary ---->")
+
+myset = set() # {,}
+hashset = set([5, 6, 5, 1]) # {6, 1, 5}
+
+myset.add(1)
+myset.add(2) # O(1) adding
+print(myset)
+
+print(len(myset))
+print(2 in myset) # O(1) look up
+
+myset.remove(1) # O(1) removeal
