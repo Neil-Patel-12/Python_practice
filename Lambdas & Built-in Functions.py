a3ddd633e733b9000fb1dec8d978bc604cf3fdcb
Neil Patel <neilpatel235@gmail.com>
2024-05-14 15:33:46 -0400

    added sort()

--- Lambdas & Built-in Functions.py
+++ Lambdas & Built-in Functions.py
@@ -3,9 +3,8 @@ import sys
 
 def square(num):
     return num * num
-
-
 print(square(9))  # 81
+
 
 square2 = lambda num: num * num
 add = lambda a, b: a + b
@@ -13,9 +12,12 @@ add = lambda a, b: a + b
 print(square2(7))  # 49
 print(add(2, 5))  # 7
 
-print(square.__name__)
-print(square2.__name__)
-print(add.__name__)
+
+print(square.__name__)  # this is just a function
+print(square2.__name__) # <lambda>
+print(add.__name__) # <lambda>
+
+
 
 
 # map: A standard function that accepts at least two arguments, a function and an "iterable"
@@ -28,8 +30,8 @@ def decrement_list(arr):
     return new_arr
 
 
-def extract_full_name(l):
-    return list(map(lambda val: f"{val['first']} {val['last']}", l))
+def extract_full_name(diction):
+    return list(map(lambda val: f"{val['first']} {val['last']}", diction))
 
 
 nums = [2, 4, 6, 8, 10]
@@ -37,12 +39,17 @@ double = map(lambda x: x * 2, nums)  # map object that hold [4, 8, 12, 16, 20]
 mapObjectList = list(double)  # or list(map(lambda x: x*2, nums))
 print(mapObjectList)
 
-names = [{"first": "Neil", "last": "Patel"},
-         {"first": "Deep", "last": "Smart"},
-         {"first": "Raj", "last": "plane"}
-         ]
+names = [
+    {"first": "Neil", "last": "Patel"},
+    {"first": "Deep", "last": "Smart"},
+    {"first": "Raj", "last": "plane"}
+]
 first_name = list(map(lambda name: name["first"].upper(), names))
 print(first_name)
+
+print(extract_full_name(names))
+
+
 
 
 # filter: There is a lambda for each value in the iterable.
@@ -63,6 +70,9 @@ names = ["Lassie", "Colt", "Rusty", "Rob", "Nick"]
 names_less_than_5 = list(map(lambda name: f"Your less than 5 {name}",
                              filter(lambda value: len(value) < 5, names)))
 print(names_less_than_5)
+
+
+
 
 # all: Return True if all elements of the iterable are truthy (or if iterable is empty)
 # any: Return True if any element of the iterable is truthy. it iterable is empty, r False
@@ -97,14 +107,32 @@ users = [
 another1 = sorted(users, key=lambda user: user["username"])
 another2 = sorted(users, key=lambda user: len(user["tweets"]))
 
+
+
+
+# sort() - will sort the array in place
+val = [4, 5, 6, 1, 2, 3, 7, 8, 9]
+val.sort()
+print(val)
+
+fruits = ["watermelons", "orange", "grape", "apple", "kiwi", 'pineapple']
+fruits.sort()
+print(fruits)
+
+fruits.sort(key=lambda fru: len(fru))
+print(fruits)
+
+
+
+
 # max: Return the larges item in an iterable or the larges of two or more arguments.
 # min is the opposite
-print(max(3, 66, 99))
-print(max('c', 'g', 'k'))
-print(max([3, 2, 4, 1]))
-print(max((1, 5, 9, 6)))
-print(max("awesome"))
-print(max({1: 'a', 3: 'c', 2: 'b'}))
+print(max(3, 66, 99))     # 99
+print(max('c', 'g', 'k')) # 'k'
+print(max([3, 2, 4, 1]))  # 4
+print(max((1, 5, 9, 6)))  # 9
+print(max("awesome"))     # 'w'
+print(max({1: 'a', 3: 'c', 2: 'b'})) # 
 
 names2 = ['Arya', 'Samson', 'Dora', 'Tim', 'Ollivander']
 most_char = max(len(name) for name in names2)  # Generator Expression
@@ -120,22 +148,29 @@ songs = [
 least_played_song = min(songs, key=lambda s: s["playcount"])
 best_song = max(songs, key=lambda s: s["playcount"])["title"]
 
+
+
+
 # reversed: Return a revers iterator
 # this will reverse any iterator
 nuns = [1, 2, 3, 4]
 nuns.reverse()  # reverse() only works with list Permanent
 print(nuns)
 
-print(list(reversed(nuns)))  # nuns = [4, 3, 2, 1]
+print(list(reversed(nuns)))  # nuns = [4, 3, 2, 1] then -> [1, 2, 3, 4]
 
 for char in reversed("hello world"):
     print(char)
 for i in reversed(range(0, 10)):
     print(i)
 
+
+
 # len: return the length      they all have a __len__() dunder
 # accept a sequence such as (list, string, tuple, range)
 # accept a collection such as (dict or set)
+
+
 
 
 # abs: Return the absolute value of a num. The argument may be an integer or a float
@@ -152,6 +187,8 @@ print(sum(simple_list))
 print(round(10.2))  # 10
 print(round(10.5))  # 11
 print(round(1.212121, 2))  # 1.21
+
+
 
 
 # zip: Make an iterator that aggregates elements from each of the iterables
@@ -171,6 +208,8 @@ def interleave(str1, str2):
 first_zip = list(zip([1, 2, 3], [4, 5, 6]))  # [(1, 4), (2, 5), (3, 6)]
 
 back_together = list(zip(*first_zip))  # [(1,2,3), (4,5,6)]
+
+
 
 midterms = [80, 91, 78]
 finals = [98, 89, 78]
