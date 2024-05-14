3a13a84f5f0a967aed0d507e6952989817cde712
Neil Patel <neilpatel235@gmail.com>
2024-05-14 14:07:46 -0400

    Common math to know

---
+++ math.py
@@ -0,0 +1,22 @@
+import math
+
+print(5 / 2)    # 2.5
+print(5 // 2)   # 2
+print(-3 // 2)  # -2, the // operator will always round down
+print(int(-3 / 2)) # -1, int(-1.5) will round to zero = -1
+
+print(5 % 3) # 2
+
+
+print(math.fmod(-5, 3))  # -2
+
+print(math.floor(1.5))   # 1
+print(math.ceil(1.5))    # 2
+print(math.sqrt(2))      # 1.4
+print(math.pow(5, 2))    # 25
+
+# infinity and -infinity
+float("int")
+float("-inf")
+
+# python number are infinite & never overflow
